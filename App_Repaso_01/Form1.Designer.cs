22fc438dd6ea16a454d30eb2fe2fc9f43b006a72
NickX zXn <gamerplayernick714@hotmail.com>
2021-11-30 15:34:39 -0500

    Agregar archivos de proyecto.

---
+++ App_Repaso_01/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace App_Repaso_01
+{
+    partial class frmRepaso
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelmain = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.listMatricula = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelmain
+            // 
+            this.labelmain.AutoSize = true;
+            this.labelmain.Font = new System.Drawing.Font("Sitka Small", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.labelmain.Location = new System.Drawing.Point(209, 28);
+            this.labelmain.Name = "labelmain";
+            this.labelmain.Size = new System.Drawing.Size(385, 35);
+            this.labelmain.TabIndex = 1;
+            this.labelmain.Text = "INSCRIPCIÓN AL SEMINARIO";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(556, 95);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(209, 36);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Enabled = false;
+            this.btnEliminar.Location = new System.Drawing.Point(556, 154);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(209, 36);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(556, 212);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(209, 36);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(556, 347);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(209, 36);
+            this.btnCerrar.TabIndex = 5;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // listMatricula
+            // 
+            this.listMatricula.FormattingEnabled = true;
+            this.listMatricula.ItemHeight = 15;
+            this.listMatricula.Location = new System.Drawing.Point(51, 127);
+            this.listMatricula.Name = "listMatricula";
+            this.listMatricula.Size = new System.Drawing.Size(438, 214);
+            this.listMatricula.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nombre";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(291, 95);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(198, 23);
+            this.txtnombre.TabIndex = 8;
+            // 
+            // txttotal
+            // 
+            this.txttotal.Location = new System.Drawing.Point(291, 360);
+            this.txttotal.Name = "txttotal";
+            this.txttotal.Size = new System.Drawing.Size(198, 23);
+            this.txttotal.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 360);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Total de Matriculados";
+            // 
+            // frmRepaso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txttotal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listMatricula);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.labelmain);
+            this.Name = "frmRepaso";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelmain;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.ListBox listMatricula;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txttotal;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ App_Repaso_01/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_Repaso_01
+{
+    public partial class frmRepaso : Form
+    {
+        public frmRepaso()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            
+            if (txtnombre.Text=="")
+            {
+                MessageBox.Show("Agrege un Nombre");
+                txtnombre.Focus();
+            }
+            else
+            {
+                string name = txtnombre.Text.ToUpper();
+                listMatricula.Items.Add(name);
+                txtnombre.Clear();
+                txtnombre.Focus();
+                btnEliminar.Enabled = true;
+            }
+            txttotal.Text = listMatricula.Items.Count.ToString("n0");
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (listMatricula.Items.Count == 0)
+            {
+                btnEliminar.Enabled = false;
+                return;
+            }
+            else if (listMatricula.Items.Count == -1)
+            {
+                MessageBox.Show("Seleccione un Elemento");
+                listMatricula.Focus();
+            }
+            else
+            {
+                listMatricula.Items.RemoveAt(listMatricula.SelectedIndex);
+                txttotal.Text = listMatricula.Items.Count.ToString("n0");
+            }
+        }
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            listMatricula.Items.Clear();
+            txtnombre.Focus();
+            txttotal.Text = listMatricula.Items.Count.ToString("n0");
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Desea Salir?", "Alerta", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                this.Close();
+            }      
+        }
+    }
+}
